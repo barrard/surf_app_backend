1ded294710a81b7626b9738d5e405b8f3d3e411a
David Barrar <barrard@gmail.com>
2019-09-20 19:32:58 -1000

    adding origin whitelist

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-require("dotenv").config();
-require("./utils/logger.js");
-const express = require('express');
-const app = express();
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const loger = require('morgan');
-const session = require("express-session");
+require('dotenv').config()
+require('./utils/logger.js')
+const express = require('express')
+const app = express()
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const loger = require('morgan')
+const session = require('express-session')
 // 1 - importing dependencies
-const passport = require("passport");
-const cors = require("cors");
-
+const passport = require('passport')
+const cors = require('cors')
+const origin_whitelist = [
+  'http://localhost:3000', 'https://waves.dakine.website'
+]
 const corsMiddleware = cors({
-  origin: "http://localhost:3000",
+  origin: (origin, callback) => {
+    if (origin_whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   credentials: true,
   preflightContinue: false
-});
-app.use(corsMiddleware);
-app.options(corsMiddleware);
+})
+app.use(corsMiddleware)
+app.options(corsMiddleware)
 
-const indexRouter = require('./routes/indexRoutes');
-const usersRouter = require('./routes/usersRoutes');
-const waveDataRouter = require('./routes/waveDataRoutes');
+const indexRouter = require('./routes/indexRoutes')
+const usersRouter = require('./routes/usersRoutes')
+const waveDataRouter = require('./routes/waveDataRoutes')
 
 
-app.use(loger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(loger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/wavedata', waveDataRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/wavedata', waveDataRouter)
 
-module.exports = app;
+module.exports = app
